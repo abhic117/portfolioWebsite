12ffa18db7e9ac464fc338aeca7ab89396cdf7c9
Abhishek Chand <abhishekc117@hotmail.com>
2024-12-07 00:10:51 +1100

    started skills page. created js code to load all skills from a text file

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const skills = document.getElementById("skills-text")
+const skills = document.getElementById("skills-text");
+const skillsContainer = document.getElementById("skills-container");
 
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
@@ -13,7 +14,14 @@ function displaySkills(file) {
     fetch(file)
     .then(res => res.text())
     .then(text => text.split("\n").forEach(skill => {
-        console.log(skill)
+        console.log(skill);
+
+        const skill_card = document.createElement('div');
+        skill_card.setAttribute('class', 'details-container');
+        skill_card.setAttribute('id', 'skill-card');
+
+        skill_card.innerHTML = `<h3>${skill}</h3>`;
+        skillsContainer.append(skill_card);
     }))
     
 }
