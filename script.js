2773fdff7c2211ca8efd2d7a862f7f6098f20465
Abhishek Chand <abhishekc117@hotmail.com>
2024-12-06 23:06:03 +1100

    about page html and styling created

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links")
-    const icon = document.querySelector(".hamburger-icon")
+const skills = document.getElementById("skills-text")
 
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+displaySkills('./assets/skills.txt');
+function displaySkills(file) {
+    fetch(file)
+    .then(res => res.text())
+    .then(text => text.split("\n").forEach(skill => {
+        console.log(skill)
+    }))
+    
 }
